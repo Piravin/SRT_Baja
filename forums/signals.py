ed2de5158397e32f2c470e35f4de8eaae313fa01
Piravin <piravin.kcm@gmail.com>
2020-08-30 19:49:59 +0530

    To be deployed

--- forums/signals.py
+++ forums/signals.py
@@ -6,6 +6,8 @@ from django.core.mail import EmailMessage
 from django.conf import settings 
 from django.urls import reverse
 
+EMAILS = [user.emai for user in User.objects.all()]
+
 def post_mail(sender,instance, created, **kwargs):
     if created:
         post_title, post_description = instance.title, instance.short_description
@@ -13,7 +15,7 @@ def post_mail(sender,instance, created, **kwargs):
         body_text = f"A new post on {post_title} was created \n A short description about the post: \n {post_description} \n Do check out the full post on srtbaja.in"
         body = f"<p>A new post on {post_title} was created</p><p> A short description about the post: </p><p> {post_description}</p><a href = 'http://srtbaja.in/forums/post/{instance.id}'>View post</a>"
         from_email = settings.EMAIL_HOST_USER
-        to_mail = ['122004189@sastra.ac.in','jujukcmp@gmail.com']
+        to_mail = EMAILS
         msg = EmailMessage(subject, body, from_email, to_mail)
         msg.content_subtype = 'html'
         msg.send(fail_silently=True)
